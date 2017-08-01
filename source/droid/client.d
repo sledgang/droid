711833717830185b00e0926632b56cc54ab205df
unleashy <lakeavenger@gmail.com>
2017-07-31 22:48:49 -0300

    Add a basic Client
    The Client is basically a glue between the Gateway, the API and the Cache in a nice, user-friendly, high-level interface.

--- source/droid/client.d
+++ source/droid/client.d
@@ -1,3 +1,74 @@
 module droid.client;
 
+import std.typecons,
+       std.variant;
 
+import droid.gateway,
+       droid.api,
+       droid.cache;
+
+struct Config
+{
+    string token;
+    API api = null;
+    Gateway gateway = null;
+    Cache cache = null;
+
+    static Config mergeWithDefaults(Config other)
+    {
+        auto api     =     other.api is null ? new DefaultAPI(other.token) : other.api;
+        auto gateway = other.gateway is null ? new Gateway(api)            : other.gateway;
+
+        return Config(
+            other.token,
+            api,
+            gateway,
+            other.cache is null ? new MemoryCache() : other.cache
+        );
+    }
+}
+
+class Client
+{
+    private API api_;
+    private Gateway gateway_;
+    private DiscordCache cache_;
+
+    this(Config config)
+    {
+        setupFromConfig(Config.mergeWithDefaults(config));
+    }
+
+    this(in string token)
+    {
+        this(tokenizedConfig(token));
+    }
+
+    void run(bool blocking = true)
+    {
+        gateway_.connect(blocking);
+    }
+
+    void block()
+    {
+        import vibe.core.core : runEventLoop;
+
+        runEventLoop();
+    }
+
+    private void setupFromConfig(Config config)
+    {
+        api_     = config.api;
+        gateway_ = config.gateway;
+        cache_   = new DiscordCache(config.cache, api_);
+    }
+
+    private Config tokenizedConfig(in string token) const
+    {
+        auto conf = Config();
+
+        conf.token = token;
+
+        return conf;
+    }
+}
